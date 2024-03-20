a9d8c5e2e576fb6d049d86e7c255418273186df6
Ishita Patel <144733216+IshitaPatel18@users.noreply.github.com>
2024-03-19 18:35:59 -0600

    fixed error on a test

--- code.test.js
+++ code.test.js
@@ -46,7 +46,7 @@ let houseExtra = [
 ];
 
 assert(JSON.stringify(dijkstra(classGraph, 2)) == JSON.stringify([9,11,0,13,8,11,9,14]));
-assert(JSON.stringify(dijkstra(classGraph, 4)) == JSON.stringify([9,11,0,13,8,11,9,14]));
+assert(JSON.stringify(dijkstra(classGraph, 4)) == JSON.stringify([18,20,9,7,0,3,1,6]));
 assert(JSON.stringify(dijkstra(ferrisWheel, 0)) == JSON.stringify([0,3,4,5,7,1,1]));
 assert(JSON.stringify(dijkstra(ferrisWheel, 5)) == JSON.stringify([8,4,12,13,15,0,1]));
 assert(JSON.stringify(dijkstra(house, 1)) == JSON.stringify([12,0,10,3,3,6,1]));
